3123c7bbd0b2d4124a456b0d1664dc389988f832
xii5ii <142681978+xii5ii@users.noreply.github.com>
2023-09-04 18:59:44 +0200

    تم حل الكلاس ورك

---
+++ script.js
@@ -0,0 +1,27 @@
+alert('مرحبا بك في موقعي انا منيره وسوف احسب درجتك');
+let grade =parseInt(prompt)("ادخل درجتك");
+ console.log(grade);
+
+ if(grade>=90 && grade <=100){
+    console.log("لقد حصلت على امتياز")
+ } else 
+
+ if(grade>=80 && grade <=89){
+    console.log("لقد حصلت على جيد جدا")
+ } else
+
+ if(grade>=70 && grade <=79){
+    console.log("لقد حصلت على جيد")
+ }else
+
+ if(grade>=60 && grade <=69){
+    console.log("لقد حصلت على مقبول")
+ }else
+
+ if(grade>=50 && grade <=59){
+    console.log("لقد حصلت على ضعيف")
+ }else
+
+ if(grade <50 && grade <=50){
+    console.log("راسب")
+ }
